82f60eb9690268dcaf736f47225ef1c403b21112
Stas <stas0412@gmail.com>
2017-11-13 09:14:37 +0200

    template files

---
+++ src/Transliteration.php
@@ -0,0 +1,12 @@
+<?php
+namespace doka\transliteration;
+
+class Transliteration
+{
+   
+    public function __construct(array $streams = [])
+    {
+        echo "class transliteration created";
+    }
+
+}
