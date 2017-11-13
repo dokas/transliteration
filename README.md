f91e59d4c156d5abdfe2c01671585d5d6cb15e59
Stas <stas0412@gmail.com>
2017-11-13 09:39:51 +0200

    Full package

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# transliteration
-Transliteration
+# Transliteration class for any strings
+
+This class is used for strings transliteration 
+
+## Install
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+$ php composer.phar require --prefer-dist doka/transliteration "*"
+```
+
+or add
+
+```json
+"doka/transliteration": "*"
+```
+
+to the require section of your `composer.json` file.
+
--- src/Transliteration.php
+++ src/Transliteration.php
@@ -1,12 +1,140 @@
 <?php
 namespace doka\transliteration;
 
-class Transliteration
+class Transliteration 
 {
-   
-    public function __construct(array $streams = [])
-    {
-        echo "class transliteration created";
-    }
+	/**
+	 * System A (with diacritics).
+	 */
+	const GOST_779A = 'GOST 7.79.A';
+	/**
+	 * System B (using combinations of letters).
+	 */
+	const GOST_779B = 'GOST 7.79.B';
 
+	/**
+	 * @var array Transliteration tables of Slavic alphabets.
+	 */
+	public static $tables = array(
+		Transliteration::GOST_779A => array(
+			'А' => 'A', 'а' => 'a',
+			'Б' => 'B', 'б' => 'b',
+			'В' => 'V', 'в' => 'v',
+			'Г' => 'G', 'г' => 'g',
+			'Д' => 'D', 'д' => 'd',
+			'Е' => 'E', 'е' => 'e',
+			'Ё' => 'Ë', 'ё' => 'ë',
+			'Ж' => 'Ž', 'ж' => 'ž',
+			'З' => 'Z', 'з' => 'z',
+			'И' => 'I', 'и' => 'i',
+			'Й' => 'J', 'й' => 'j',
+			'К' => 'K', 'к' => 'k',
+			'Л' => 'L', 'л' => 'l',
+			'М' => 'M', 'м' => 'm',
+			'Н' => "N", 'н' => 'n',
+			'О' => 'O', 'о' => 'o',
+			'П' => 'P', 'п' => 'p',
+			'Р' => 'R', 'р' => 'r',
+			'С' => 'S', 'с' => 's',
+			'Т' => 'T', 'т' => 't',
+			'У' => 'U', 'у' => 'u',
+			'Ф' => 'F', 'ф' => 'f',
+			'Х' => 'H', 'х' => 'h',
+			'Ц' => 'C', 'ц' => 'c',
+			'Ч' => 'Č', 'ч' => 'č',
+			'Ш' => 'Š', 'ш' => 'š',
+			'Щ' => 'Ŝ', 'щ' => 'ŝ',
+			'Ъ' => 'ʺ', 'ъ' => 'ʺ',
+			'Ы' => 'Y', 'ы' => 'y',
+			'Ь' => 'ʹ', 'ь' => 'ʹ',
+			'Э' => 'È', 'э' => 'è',
+			'Ю' => 'Û', 'ю' => 'û',
+			'Я' => 'Â', 'я' => 'â',
+			'№' => '#', 'Ӏ' => '‡',
+			'’' => '`', 'ˮ' => '¨',
+		),
+		Transliteration::GOST_779B => array(
+			'А' => 'A', 'а' => 'a',
+			'Б' => 'B', 'б' => 'b',
+			'В' => 'V', 'в' => 'v',
+			'Г' => 'G', 'г' => 'g',
+			'Д' => 'D', 'д' => 'd',
+			'Е' => 'E', 'е' => 'e',
+			'Ё' => 'Yo', 'ё' => 'yo',
+			'Ж' => 'Zh', 'ж' => 'zh',
+			'З' => 'Z', 'з' => 'z',
+			'И' => 'I', 'и' => 'i',
+			'Й' => 'J', 'й' => 'j',
+			'К' => 'K', 'к' => 'k',
+			'Л' => 'L', 'л' => 'l',
+			'М' => 'M', 'м' => 'm',
+			'Н' => "N", 'н' => 'n',
+			'О' => 'O', 'о' => 'o',
+			'П' => 'P', 'п' => 'p',
+			'Р' => 'R', 'р' => 'r',
+			'С' => 'S', 'с' => 's',
+			'Т' => 'T', 'т' => 't',
+			'У' => 'U', 'у' => 'u',
+			'Ф' => 'F', 'ф' => 'f',
+			'Х' => 'H', 'х' => 'h',
+			'Ц' => 'Cz', 'ц' => 'cz',
+			'Ч' => 'Ch', 'ч' => 'ch',
+			'Ш' => 'Sh', 'ш' => 'sh',
+			'Щ' => 'Shh', 'щ' => 'shh',
+			'Ъ' => 'ʺ', 'ъ' => 'ʺ',
+			'Ы' => 'Y`', 'ы' => 'y`',
+			'Ь' => '', 'ь' => '',
+			'Э' => 'E`', 'э' => 'e`',
+			'Ю' => 'Yu', 'ю' => 'yu',
+			'Я' => 'Ya', 'я' => 'ya',
+			'№' => '#', 'Ӏ' => '‡',
+			'’' => '`', 'ˮ' => '¨',
+		),
+	);
+
+	/**
+	 * @var string
+	 */
+	private $_standard = Transliteration::GOST_779B;
+
+	/**
+	 * @param $value
+	 * @throws CException
+	 */
+	public function setStandard($value)
+	{
+		if(!isset(Transliteration::$tables[$value])) {
+			throw new CException(Yii::t('yiiext', 'Invalid Transliteration standard {standard}', array(
+					'{standard}' => $value,
+				)));
+		}
+		$this->_standard = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStandard()
+	{
+		return $this->_standard;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getTransliterationTable()
+	{
+		return Transliteration::$tables[$this->standard];
+	}
+
+	/**
+	 * Transliterate into Latin characters of Cyrillic characters.
+	 * @param string $content the content to be transliterate.
+	 * @return string the transliterated content
+	 */
+	public function transliterate($content)
+	{
+		return str_replace(array_keys($this->getTransliterationTable()), array_values($this->getTransliterationTable()), $content);
+	}
 }
+
